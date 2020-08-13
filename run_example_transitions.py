1d4927d2bbf0bb22351b7479fdd3d1d11b4036ad
Berlin Del Aguila <bad413@humboldt.edu>
2020-08-13 00:12:42 -0700

    new file:   run_example_transitions.py 	new file:   testing/static_environment/Illustrate_copy.py 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_12_07_32_08.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_12_18_43_12.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_00_59_41.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_01_00_35.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_01_04_06.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_01_06_25.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_01_15_05.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_01_15_23.dat 	new file:   testing/static_environment/Static_Obstacles/goal_2020_08_13_01_15_36.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_12_07_32_08.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_12_18_43_12.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_00_59_41.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_01_00_35.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_01_04_06.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_01_06_25.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_01_15_05.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_01_15_23.dat 	new file:   testing/static_environment/Static_Obstacles/propositions_2020_08_13_01_15_36.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_12_07_32_08.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_12_18_43_12.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_00_59_41.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_01_00_35.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_01_04_06.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_01_06_25.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_01_15_05.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_01_15_23.dat 	new file:   testing/static_environment/Static_Obstacles/static_obstacle_matrix_2020_08_13_01_15_36.dat

---
+++ run_example_transitions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 16:25:07 2020
+
+@author: berlindelaguila
+"""
+import sys 
+import os
+import test_run_configuration_transitions
+
+sys.path.append('/Users/berlindelaguila/AutoValetParking_TE/testing/static_environment/transitions')
+
+
+import Illustrate_copy
+
+Illustrate_copy.static_example()
+
+#sys.path.append('/Users/berlindelaguila/AutoValetParking/testing/AVP_berlin')
+#import sys_ForLoops 
